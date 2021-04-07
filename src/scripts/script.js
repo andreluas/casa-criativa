f39b6326883544aaf75712473f101b50fd9ab71f
andreluas <andreluas.k@gmail.com>
2021-04-07 23:18:29 +0000

    function onOff ok

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,10 @@
+function onOff(){
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+}
+
 document
     .querySelector("button.fat")
-    .addEventListener("click", function(){
-        document
-            .querySelector("#footer")
-            .classList
-            .toggle("hide")
-    })
+    .addEventListener("click", onOff)
