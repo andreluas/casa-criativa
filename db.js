914693c55617ec953f078e5ec300a1d4f6e917cf
andreluas <andreluas.k@gmail.com>
2021-04-10 21:49:43 -0300

    configure database :blue_book:

---
+++ db.js
@@ -0,0 +1,57 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./casa-criativa.db')
+
+db.serialize(function(){
+    // Criar tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT,
+            category TEXT,
+            description TEXT,
+            link TEXT
+        );
+    `)
+    // Inserir dados
+    // const query = `
+    // INSERT INTO ideas(
+    //         image,
+    //         title,
+    //         category,
+    //         description,
+    //         link
+    //     ) VALUES (?,?,?,?,?);
+    // `
+
+    // const values = [
+    //     "https://www.flaticon.com/svg/vstatic/svg/1005/1005141.svg?token=exp=1618005864~hmac=7a4a4ea021ea06c80c6f618356343ce0",
+    //     "Cursos de Programação",
+    //     "Estudo",
+    //     "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
+    //     "https://rocketseat.com.br"
+    // ]
+
+    // db.run(query, values, function(err){
+    //     if(err) return console.log(err)
+
+    //     console.log(this)
+    // })
+    
+    // Deletar dados
+    // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err){
+    //     if(err) return console.log(err)
+
+    //     console.log("DELETE SUCESS", this)
+    // })
+    
+    // Consulta de dados
+    // db.all(`SELECT * FROM ideas`, function(err, rows){
+    //     if(err) return console.log(err)
+
+    //     console.log(rows)
+    // })
+    
+})
+
+module.exports = db
--- server.js
+++ server.js
@@ -2,37 +2,39 @@
 const express = require("express")
 const server = express()
 
+const db = require("./db")
+
 // Parametrização de Ideas
-const ideas = [
-    {
-        img: "https://www.flaticon.com/svg/vstatic/svg/1005/1005141.svg?token=exp=1618005864~hmac=7a4a4ea021ea06c80c6f618356343ce0",
-        title: "Cursos de Programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://www.flaticon.com/svg/vstatic/svg/2936/2936886.svg?token=exp=1618005940~hmac=245f288b484f69aedac9143c20e93c1a",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://www.flaticon.com/svg/vstatic/svg/2906/2906496.svg?token=exp=1618005981~hmac=3cfeedb1d7b3d1adfb5d8429aa5aa83c",
-        title: "Meditação",
-        category: "Espiritualidade",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://www.flaticon.com/svg/vstatic/svg/3227/3227649.svg?token=exp=1618006015~hmac=f2540146ffa3f52fe1f687693769c01e",
-        title: "Desenho & Pintura",
-        category: "Arte",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
-        url: "https://rocketseat.com.br"
-    },
-]
+// const ideas = [
+//     {
+//         img: "https://www.flaticon.com/svg/vstatic/svg/1005/1005141.svg?token=exp=1618005864~hmac=7a4a4ea021ea06c80c6f618356343ce0",
+//         title: "Cursos de Programação",
+//         category: "Estudo",
+//         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
+//         url: "https://rocketseat.com.br"
+//     },
+//     {
+//         img: "https://www.flaticon.com/svg/vstatic/svg/2936/2936886.svg?token=exp=1618005940~hmac=245f288b484f69aedac9143c20e93c1a",
+//         title: "Exercícios",
+//         category: "Saúde",
+//         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
+//         url: "https://rocketseat.com.br"
+//     },
+//     {
+//         img: "https://www.flaticon.com/svg/vstatic/svg/2906/2906496.svg?token=exp=1618005981~hmac=3cfeedb1d7b3d1adfb5d8429aa5aa83c",
+//         title: "Meditação",
+//         category: "Espiritualidade",
+//         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
+//         url: "https://rocketseat.com.br"
+//     },
+//     {
+//         img: "https://www.flaticon.com/svg/vstatic/svg/3227/3227649.svg?token=exp=1618006015~hmac=f2540146ffa3f52fe1f687693769c01e",
+//         title: "Desenho & Pintura",
+//         category: "Arte",
+//         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, pariatur autem quibusdam labore quis mollitia consequatur harum hic cum aspernatur minima asperiores placeat, facilis magnam ratione ab et error soluta?",
+//         url: "https://rocketseat.com.br"
+//     },
+// ]
 
 // Configuração de arquivos estáticos (css, scripts, imgs)
 server.use(express.static("public"))
