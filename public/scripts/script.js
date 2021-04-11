5082eee178d906f83f8e02d9afaf29b3113e57d7
andreluas <andreluas.k@gmail.com>
2021-04-10 22:54:46 -0300

    function isEmpty in modal ok :heavy_check_mark:

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -18,11 +18,23 @@ function onOff(){
 function checkFields(event) {
     const valuesToCheck = [
         "title",
-        "image",
         "category",
+        "image",
         "description",
         "link",
     ]
 
-    
+    const isEmpty = valuesToCheck.find(function(value) {
+        const checkIfIsString = typeof event.target[value].value === "string" 
+        const checkIfIsEmpty = !event.target[value].value.trim()
+        
+        if(checkIfIsString && checkIfIsEmpty){
+            return true
+        }
+    })
+
+    if(isEmpty) {
+        event.preventDefault()
+        alert("Preencha todos os campos!")
+    }
 }
